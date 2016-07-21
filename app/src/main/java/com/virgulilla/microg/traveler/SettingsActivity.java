488e4d5afe0fa736d9f529b255fd0fb47a8c1855
Roboe <roboe@virgulilla.com>
2016-07-21 15:14:08 +0200

    Add rough UI

--- app/src/main/java/com/virgulilla/microg/traveler/SettingsActivity.java
+++ app/src/main/java/com/virgulilla/microg/traveler/SettingsActivity.java
@@ -2,25 +2,51 @@ package com.virgulilla.microg.traveler;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    double[] coordinates;
+    @BindView(R.id.latitude_origin)
+    EditText etLatitudeOrigin;
+    @BindView(R.id.longitude_origin)
+    EditText etLongitudeOrigin;
+
+    private LocationPrefs prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+
+        prefs = LocationPrefs.getInstance(getApplicationContext());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        LocationPrefs prefs = LocationPrefs.getInstance(getApplicationContext());
-        coordinates = prefs.getOriginLocation();
+        fillFields(prefs.getOriginLocation());
+    }
 
-        prefs.setOriginLocation(40.42412, -3.71733); // Templo de Debod
-        coordinates = prefs.getOriginLocation();
+    @OnClick(R.id.update_location)
+    public void submit(View view) {
+        double lat = Double.parseDouble(etLatitudeOrigin.getText().toString());
+        double lon = Double.parseDouble(etLongitudeOrigin.getText().toString());
+
+        prefs.setOriginLocation(lat, lon);
+
+        fillFields(prefs.getOriginLocation());
+    }
+
+    private void fillFields(double[] coordinates) {
+        etLatitudeOrigin.setText(Double.toString(coordinates[0]));
+        etLongitudeOrigin.setText(Double.toString(coordinates[1]));
     }
 }
